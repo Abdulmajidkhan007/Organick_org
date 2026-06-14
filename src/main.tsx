64782cd81bfefec51081ec161202f6a88308a478
Abdulloh <abdullohsharipov77700@gmail.com>
2026-06-14 10:59:28 +0500

    fix: switch FontAwesome to CDN v6, add missing Firebase env vars

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.css'
 import './Fonts.css'
-import '@fortawesome/fontawesome-free/css/all.min.css'
+// FontAwesome loaded via CDN in index.html (v6.7.2 - compatible with fas/far/fab classes)
+// import '@fortawesome/fontawesome-free/css/all.min.css'
 import './i18n/index'
 import { App } from './App'
 
